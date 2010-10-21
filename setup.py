614344fb7e7396a8bd82e8cfcf47e152e62bedd9
Alon Horev <alonho@gmail.com>
2010-10-21 20:38:04 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name="sgpy",
+      classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.6"],
+      description="Framework for manipulation and execution of SCSI commands using the Linux SG driver",
+      license="BSD",
+      author="Alon Horev",
+      author_email="alonho@gmail.com",
+      url="http://github.com/alonho/sgpy",
+      version="0.3",
+      packages=["sgpy"],
+      requires=["construct"]
+      )
