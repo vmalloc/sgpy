119336f7b387d94f5a6aac8c2e0fe05a467e7f05
Alon Horev <alonho@gmail.com>
2010-10-21 16:37:17 +0200

    added some metadata: author + version

--- __init__.py
+++ __init__.py
@@ -2,3 +2,6 @@ from .channel import Channel
 from .reactor import SelectReactor
 from .handler import *
 from . import command
+
+__author__ = "Alon Horev (alonho [at] gmail.com)"
+__version__ = "0.1"
